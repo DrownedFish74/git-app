03a13d41e8617a5aa73fb3d70b7eb2f1e4748264
DrownedFish74 <drowned.lolita@gmail.com>
2020-04-01 19:42:46 +0900

    add conflict a

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
-
+#master branch編集実験
   def index
     @articles = Article.all
   end
